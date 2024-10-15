226288522b5a687a49b3a7ddafa0e2b1888b382a
车丽钠 <changbingmushao@qq.com>
2024-10-15 20:28:21 +0800

    前后端全局变量成功

--- BlogFont/src/main.js
+++ BlogFont/src/main.js
@@ -6,6 +6,10 @@ import router from './router'
 
 const app = createApp(App).use(router)
 
-app.config.globalProperties.URL = 'http://127.0.0.1:8081';
+// 重构前的后端，仅有一个main.go，Back文件，已弃用
+// app.config.globalProperties.URL = 'http://127.0.0.1:8081';
+
+// 重构后的后端，文件分类管理，BlogBack
+app.config.globalProperties.URL = 'http://127.0.0.1:3000/api';
 
 app.mount('#app')
